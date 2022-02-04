5687e52a2cbe53f53ecc6b84342dc93115f6f163
leesh125 <chelsea12345@naver.com>
2022-02-04 20:17:47 +0900

    상품 도메인 개발 - 상품 도메인 생성 및 테스트

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,27 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+//@Getter @Setter
+@Data // Getter, Setter @ToString 등등
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
