87c62c43136637779a4b6d95d4a9c3ef31dda427
leesh125 <chelsea12345@naver.com>
2022-02-17 20:26:34 +0900

    프로젝트 준비 V2 - 스프링이 제공하는 검증 오류처리를 사용하기 위한 setting

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemControllerV2.java
@@ -0,0 +1,102 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("form/v2/items")
+@RequiredArgsConstructor
+public class BasicItemControllerV2 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "form/v2/items";
+    }
+
+
+    @GetMapping("/{itemId}")
+    public String Item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "form/v2/item";
+    }
+
+    // 상품 추가버튼을 누르면 실행
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+
+        return "form/v2/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){ // 상품명 공백 입력 경우
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다");
+        }
+        if (item.getPrice() == null || item.getPrice()<0 || item.getPrice() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000 ) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어햐 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()){
+            log.info("errors = {}", errors);
+            model.addAttribute("errors",errors);
+            return "form/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); // redirect 반환에 치환된다.
+        redirectAttributes.addAttribute("status", true); // 남은 친구들은 쿼리파라미터 형식으로
+        return "redirect:/form/v2/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "form/v2/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/form/v2/items/{itemId}";
+    }
+
+}
