f3c1deec033ac6b04c0b25d3df121cc280734d2f
leesh125 <chelsea12345@naver.com>
2022-02-18 00:16:00 +0900

    오류 코드와 메시지 처리5 - 1. rejectValue() 호출   2. MessageCodesResolver 를 사용해서 검증 오류 코드로 메시지 코드들을 생성   3. new FieldError() 를 생성하면서 메시지 코드들을 보관   4. th:erros 에서 메시지 코드들로 메시지를 순서대로 메시지에서 찾고, 노출

--- src/main/java/hello/itemservice/web/basic/BasicItemControllerV2.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -165,10 +166,13 @@ public class BasicItemControllerV2 {
         log.info("objectNames={}", bindingResult.getObjectName());
         log.info("target={}", bindingResult.getTarget());
 
-        // 검증 로직 // BindingResult == error
+        // 검증 로직
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
+
+       /* 위 코드와 동일
         if(!StringUtils.hasText(item.getItemName())){ // 상품명 공백 입력 경우
             bindingResult.rejectValue("itemName","required");
-        }
+        }*/
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
             bindingResult.rejectValue("price","range", new Object[]{1000, 1000000}, null);
         }
