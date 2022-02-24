9129878f547245c3a728657cf1017f9a1e824e14
leesh125 <chelsea12345@naver.com>
2022-02-24 12:19:58 +0900

    Bean Validation - ValidatorFactory 에서 Validator를 가져오고 검증을 수행해보면 ConstraintViolation에 오류사항이 담긴다

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,7 +3,11 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
@@ -11,8 +15,16 @@ import java.util.List;
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백 X") // 빈 값 + 공백 허용 안함(메시지 기본 설정)
     private String itemName;
+
+    @NotNull // null을 허용 안 함
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
